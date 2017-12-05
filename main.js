64411ed43faa49491403a36ac0f6a6c7b9877e4b
armanrozika <padahariyanglain@gmail.com>
2017-12-05 07:58:20 +0700

    add more style to functionality

--- main.js
+++ main.js
@@ -1,13 +1,18 @@
 
-document.getElementById('button').addEventListener('click', function(){
+document.getElementById('button').addEventListener('click', getData);
+document.getElementById('input').addEventListener('keydown', function(e){
+  if (e.keyCode == 13){
+    getData();
+  }
+});
+
+function getData(){
   var loading = document.getElementById('wait');
   loading.style.display = "block";
   var word = document.getElementById('input').value;
   var apiUrl1 = "http://api.wordnik.com:80/v4/word.json/";
   var apiUrl2 ="/definitions?limit=200&includeRelated=true&sourceDictionaries=webster&useCanonical=false&includeTags=false&api_key=63280c6c8c8196b97200707d5db0a2cc9bf0047fa36011ef9";
-
   var getData = apiUrl1 + word + apiUrl2;
-
   var myRequest = new XMLHttpRequest();
   myRequest.open('GET', getData);
   myRequest.onload = function (){
@@ -15,12 +20,14 @@ document.getElementById('button').addEventListener('click', function(){
     loading.style.display = "none";
     var myData = JSON.parse(myRequest.responseText);
     var container = document.getElementById('result');
-    //var resultText = document.createElement('p');
     container.innerText = myData[0].text;
-
+    var title = document.getElementById('title');
+    title.innerText = word;
   };
-
-
   myRequest.send();
+  var input = document.getElementById('input');
+    input.value = '';
+}
 
-});
+
+
